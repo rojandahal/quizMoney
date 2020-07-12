b2d08137e4f9faee2890acf43bdacb92048ad8bc
rojandahal <maxpanye60@gmail.com>
2020-07-12 13:06:44 +0545

    First Commit

---
+++ app/src/main/java/com/project/quizmoney/ui/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.project.quizmoney.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.quizmoney.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SplashScreen.this.startActivity(new Intent(this,MainActivity.class));
+
+    }
+}
