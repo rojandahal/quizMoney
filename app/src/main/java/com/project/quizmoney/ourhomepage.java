4890feb90010938536d6202130fab68675845c72
sujanxtha <suzan394607@gmail.com>
2020-07-15 13:33:02 +0545

    "homepage"

---
+++ app/src/main/java/com/project/quizmoney/ourhomepage.java
@@ -0,0 +1,14 @@
+package com.project.quizmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ourhomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ourhomepage);
+    }
+}
